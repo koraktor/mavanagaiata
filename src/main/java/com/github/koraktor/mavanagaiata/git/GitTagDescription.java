33d3986e9f64e85e3267cf11d58c67a6b31da514
Sebastian Staudt <koraktor@gmail.com>
2012-12-10 08:12:52 +0100

    Added missing class GitTagDescription

---
+++ src/main/java/com/github/koraktor/mavanagaiata/git/GitTagDescription.java
@@ -0,0 +1,75 @@
+/**
+ * This code is free software; you can redistribute it and/or modify it under
+ * the terms of the new BSD License.
+ *
+ * Copyright (c) 2012, Sebastian Staudt
+ */
+
+package com.github.koraktor.mavanagaiata.git;
+
+/**
+ * Represents information about a Git commit like supplied by
+ * {@code git describe}
+ *
+ * @author Sebastian Staudt
+ */
+public class GitTagDescription {
+
+    protected String abbreviatedCommitId;
+
+    protected GitCommit commit;
+
+    protected int distance;
+
+    protected GitTag nextTag;
+
+    /**
+     * Create a new description for the given information
+     *
+     * @param repository The repository which contains the commit
+     * @param commit The commit
+     * @param nextTag The next tag reachable from the commit
+     * @param distance The distance to the next tag
+     * @throws GitRepositoryException if the commit can not be resolved
+     */
+    public GitTagDescription(GitRepository repository, GitCommit commit,
+                             GitTag nextTag, int distance)
+            throws GitRepositoryException {
+        this.abbreviatedCommitId = repository.getAbbreviatedCommitId(commit);
+        this.commit              = commit;
+        this.distance            = distance;
+        this.nextTag             = nextTag;
+    }
+
+    /**
+     * Returns the name of the next reachable tag
+     *
+     * @return The name of the next tag
+     */
+    public String getNextTagName() {
+        return (this.nextTag == null) ? "" : this.nextTag.getName();
+    }
+
+    /**
+     * Returns the string representation of this description
+     * <p>
+     * This includes the abbreviated commit ID and (if available) the distance
+     * to and the name of the next tag.
+     *
+     * @return The string representation of this description
+     */
+    @Override
+    public String toString() {
+        if (this.nextTag == null) {
+            return this.abbreviatedCommitId;
+        } else if (this.distance == 0) {
+            return this.nextTag.getName();
+        } else {
+            return String.format("%s-%d-g%s",
+                this.nextTag.getName(),
+                this.distance,
+                this.abbreviatedCommitId);
+        }
+    }
+
+}
---
+++ src/test/java/com/github/koraktor/mavanagaiata/git/GitTagDescriptionTest.java
@@ -0,0 +1,58 @@
+/**
+ * This code is free software; you can redistribute it and/or modify it under
+ * the terms of the new BSD License.
+ *
+ * Copyright (c) 2012, Sebastian Staudt
+ */
+
+package com.github.koraktor.mavanagaiata.git;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class GitTagDescriptionTest {
+
+    private GitCommit commit;
+
+    private GitRepository repository;
+
+    private GitTag tag;
+
+    @Before
+    public void setup() throws Exception {
+        this.commit = mock(GitCommit.class);
+        this.repository = mock(GitRepository.class);
+        when(this.repository.getAbbreviatedCommitId(this.commit))
+            .thenReturn("deadbeef");
+        this.tag = mock(GitTag.class);
+        when(this.tag.getName()).thenReturn("1.0.0");
+    }
+
+    @Test
+    public void test() throws Exception {
+        GitTagDescription description = new GitTagDescription(this.repository, this.commit, this.tag, 3);
+
+        assertThat(description.toString(), is(equalTo("1.0.0-3-gdeadbeef")));
+    }
+
+    @Test
+    public void testNoTag() throws Exception {
+        GitTagDescription description = new GitTagDescription(this.repository, this.commit, this.tag, 0);
+
+        assertThat(description.toString(), is(equalTo("1.0.0")));
+    }
+
+    @Test
+    public void testTagged() throws Exception {
+        GitTagDescription description = new GitTagDescription(this.repository, this.commit, null, -1);
+
+        assertThat(description.toString(), is(equalTo("deadbeef")));
+    }
+
+}
