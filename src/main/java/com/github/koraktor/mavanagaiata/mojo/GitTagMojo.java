ed204af2a36a6f629bdb6f8778387c2bc55f6334
Sebastian Staudt <koraktor@gmail.com>
2015-11-12 07:24:25 +0100

    Improve documentation of GitTagMojo
    Closes #40

--- src/main/java/com/github/koraktor/mavanagaiata/mojo/GitTagMojo.java
+++ src/main/java/com/github/koraktor/mavanagaiata/mojo/GitTagMojo.java
@@ -2,7 +2,7 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2011-2014, Sebastian Staudt
+ * Copyright (c) 2011-2015, Sebastian Staudt
  */
 
 package com.github.koraktor.mavanagaiata.mojo;
@@ -11,8 +11,10 @@ import com.github.koraktor.mavanagaiata.git.GitRepositoryException;
 import com.github.koraktor.mavanagaiata.git.GitTagDescription;
 
 /**
- * This goal provides the most recent Git tag in the "mavanagaiata.tag" and
- * "mvngit.tag" properties.
+ * This goal provides the most recent Git tag name in the
+ * "mavanagaiata.tag.name" and "mvngit.tag.name" properties. Additionally,
+ * more exact information as per <code>git describe</code> is provided in the
+ * "mavanagaiata.tag.describe" and "mvngit.tag.describe" properties
  *
  * @author Sebastian Staudt
  * @goal tag
@@ -25,7 +27,10 @@ public class GitTagMojo extends AbstractGitMojo {
     /**
      * This will first read all tags and walk the commit hierarchy down from
      * HEAD until it finds one of the tags. The name of that tag is written
-     * into "mavanagaiata.tag" and "mvngit.tag" respectively.
+     * into "mavanagaiata.tag.name" and "mvngit.tag.name" respectively.
+     * <p>
+     * A more canonical representation as per <code>git describe</code> is
+     * stored in "mavanagaiata.tag.describe" and "mvngit.tag.describe".
      *
      * @throws MavanagaiataMojoException if the tags cannot be read
      */
