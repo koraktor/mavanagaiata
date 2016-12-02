1913ded1050a2d1f9e323d5cb466e35765114dee
Sebastian Staudt <koraktor@gmail.com>
2016-12-02 06:44:33 +0100

    Replace deprecated @Component annotation

--- src/main/java/com/github/koraktor/mavanagaiata/mojo/AbstractGitMojo.java
+++ src/main/java/com/github/koraktor/mavanagaiata/mojo/AbstractGitMojo.java
@@ -13,7 +13,6 @@ import java.util.Properties;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
@@ -129,7 +128,7 @@ abstract class AbstractGitMojo extends AbstractMojo {
     /**
      * The Maven project
      */
-    @Component
+    @Parameter(defaultValue = "${project}", readonly = true)
     protected MavenProject project;
 
     /**
