d3aee065222f7542e9be0ccdd7f7dd38f6e5b0ba
Sebastian Staudt <koraktor@gmail.com>
2018-06-08 22:30:31 +0200

    Add missing import

--- src/test/java/com/github/koraktor/mavanagaiata/mojo/ChangelogMojoTest.java
+++ src/test/java/com/github/koraktor/mavanagaiata/mojo/ChangelogMojoTest.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.TimeZone;
 
 import org.junit.Before;
