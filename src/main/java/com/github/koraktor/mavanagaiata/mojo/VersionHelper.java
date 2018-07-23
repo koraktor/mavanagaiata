a83f5e80ddf079bccce9f155af7f96ad0bf78c4d
Sebastian Staudt <koraktor@gmail.com>
2018-07-23 15:25:08 +0200

    Make VersionHelper abstract

--- src/main/java/com/github/koraktor/mavanagaiata/mojo/VersionHelper.java
+++ src/main/java/com/github/koraktor/mavanagaiata/mojo/VersionHelper.java
@@ -2,7 +2,7 @@
  * This code is free software; you can redistribute it and/or modify it under
  * the terms of the new BSD License.
  *
- * Copyright (c) 2014, Sebastian Staudt
+ * Copyright (c) 2014-2018, Sebastian Staudt
  */
 
 package com.github.koraktor.mavanagaiata.mojo;
@@ -13,7 +13,7 @@ package com.github.koraktor.mavanagaiata.mojo;
  *
  * @author Sebastian Staudt
  */
-class VersionHelper {
+abstract class VersionHelper {
 
     /**
      * Returns the current version of Mavanagaiata
